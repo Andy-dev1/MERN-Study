44adc4eb62a89eeff4db37062253b3f68d9d125a
Andy-dev1 <andre.andre.jazz@gmail.com>
2023-01-03 17:12:40 -0300

    React config

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter,Navigate,Routes,Route } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+
+const App = () => {
+  return (
+    <div className='app'>
+
+    </div>
+  );
+}
+
+export default App;
